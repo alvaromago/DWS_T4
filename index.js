5ae6c03193296febf78428f167d0b6dd6e4f8287
alvaromago <aalvaromago@gmail.com>
2024-02-08 10:37:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const { MongoClient } = require("mongodb");
+
+const urlConexion = "mongodb://127.0.0.1:27017";
+const conexion = new MongoClient(urlConexion);
+
+consultar()
+	.catch(console.error)
+	.then(console.log)
+	.finally(() => conexion.close());
+
+// Actualizar
+async function actualizar() {
+	await conexion.connect();
+	const dbo = conexion.dbo("dws");
+
+	let filtro = {};
+	let valores = { $set: { centro: "Sol" } };
+	let resultado = await dbo.collection("alumno").updateOne(filtro, valores);
+	console.log(resultado);
+}
+
+// Consultar
+async function consultar() {
+	await conexion.connect();
+	const dbo = conexion.db("dws");
+
+	let filtro = { nombre: "Álvaro" };
+	let resultado = await dbo.collection("alumnos").find(filtro).toArray();
+	console.log(resultado);
+}
+
+// Insertar datos
+async function insertar() {
+	await conexion.connect();
+	const dbo = conexion.db("dws");
+
+	let datos = [
+		{ nombre: "Federico", apellidos: "Gómez" },
+		{ nombre: "Fernando", apellidos: "Rodríguez" },
+		{ nombre: "Pablo", apellidos: "González" },
+	];
+	let resultado = await dbo.collection("alumnos").insertMany(datos);
+	// let resultado = await dbo.collection("alumnos").insertOne(datos);
+	console.log("Datos insertados", resultado);
+}
+
+// Crear colección
+async function crearColeccion() {
+	await conexion.connect();
+	const dbo = conexion.db("dws");
+
+	let result = await dbo.createCollection("alumnos");
+	console.log("Colección creada: " + result.CollectionName);
+}
